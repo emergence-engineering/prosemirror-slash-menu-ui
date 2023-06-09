10003f9d9b272e6763f988b3771d6f90931fbf7e
VV-EE <67345012+VV-EE@users.noreply.github.com>
2023-06-09 16:58:25 +0200

    Initial version, loads in react app.

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import App from './App.svelte';
+
+export const SvelteApp = App;
+
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'world'
+// 	}
+// });
+
+export default App;
